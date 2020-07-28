03ceebcb52035c4277329f2fbe713c330f87996a
renyongheng <renyongheng@wangdian.cn>
2020-07-28 11:36:05 +0800

    kaifa2

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     protected $baseUrl = 'http://localhost';
 
-    /** 开发
+    /** 开发2
      * Creates the application.
      *
      * @return \Illuminate\Foundation\Application
